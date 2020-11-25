cc4b47c3a0711371a2554c06581f2edb2ea56875
GriffinMB <byatt.griffin@gmail.com>
2020-11-25 06:45:36 -0600

    github sobelow runner

--- mix.exs
+++ mix.exs
@@ -44,7 +44,8 @@ defmodule PhxDemo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:sobelow, "~> 0.10.6", github: "nccgroup/sobelow", branch: "sarif-output"}
     ]
   end
 
