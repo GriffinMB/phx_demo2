2cc3a8375026dba79cbedad6b258fdf3633bbdf3
GriffinMB <byatt.griffin@gmail.com>
2020-12-01 08:40:09 -0600

    Use master branch of sobelow

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule PhxDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:sobelow, "~> 0.10.6", github: "nccgroup/sobelow", branch: "sarif-output"}
+      {:sobelow, "~> 0.10.6", github: "nccgroup/sobelow"}
     ]
   end
 
